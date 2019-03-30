b84fb529ca1eb4b09dc02b08940149cc3c806e76
Victor Davila <bvdavila@live.com>
2019-03-30 13:56:50 -0700

    Added title screen/base scene

---
+++ Game Jam 2019/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ExitGame : MonoBehaviour
+{
+    public Button m_exit;
+
+    void Start()
+    {
+        m_exit.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+}
